a133fe266c252a674f7d40808542029899d678bc
Sinet2000 <giinger11352@gmail.com>
2026-01-18 21:45:09 +0200

    added Google Analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -348,6 +348,20 @@ export default async function LocaleLayout({ children, params }: Props) {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KFN3WCRXX5"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KFN3WCRXX5');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${inter.variable} ${jakarta.variable} font-sans antialiased`}
